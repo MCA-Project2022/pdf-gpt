651cf57e3cff05f5087633b8cef71d453636e068
Rohan Rathod <rohan.rthd.58@gmail.com>
2023-11-29 23:55:46 +0530

    Feat: Upload File Dialog Completed

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,30 +1,28 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
- 
-const f = createUploadthing();
- 
-const auth = (req: Request) => ({ id: "fakeId" }); 
- 
 
+const f = createUploadthing();
 export const ourFileRouter = {
   
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
    
     .middleware(async ({ req }) => {
-      
-      const user = await auth(req);
- 
-      if (!user) throw new Error("Unauthorized");
- 
-     
-      return { userId: user.id };
+        const {getUser} = getKindeServerSession()
+        const user = await getUser()
+        if(!user || !user.id) throw new Error("Unauthorised")
+      return {userId: user.id};
     })
     .onUploadComplete(async ({ metadata, file }) => {
-    
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      console.log("file url", file.url);
-
-      return { uploadedBy: metadata.userId };
+        const createdFile = await db.file.create({
+            data: {
+                key: file.key,
+                name: file.name,
+                userId: metadata.userId,
+                url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+                uploadStatus: 'PROCESSING',
+              },
+        })
     }),
 } satisfies FileRouter;
  
