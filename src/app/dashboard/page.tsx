da3c7ce736018273ca64064e9801f0b2d066ab2d
Hrishi151 <hrishikeshyenure@gmail.com>
2023-11-29 17:21:30 +0530

    Add funtions to dashboard(Upload file, Delete file)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,20 +4,18 @@ import { db } from '@/db'
 import Dashboard from "@/components/Dashboard"
 
 const Page = async () => {
-    const { getUser } = getKindeServerSession()
-    const user = await getUser()
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
-    if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
+  const dbUser =  db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  })
 
-    const dbUser = await db.user.findFirst({
-        where: {
-          id: user.id
-        }
-      })
-    
-      if(!dbUser) redirect('/auth-callback?origin=dashboard')
-
+  if(!dbUser) redirect('/auth-callback?origin=dashboard')
 
     return <Dashboard  />
 }
