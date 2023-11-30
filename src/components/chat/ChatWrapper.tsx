876832f29cae955fe0fade43c68fd6926525368e
Rohan Rathod <rohan.rthd.58@gmail.com>
2023-11-30 11:09:43 +0530

    Add Loading State

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -29,7 +29,7 @@ const ChatWrapper = ({
       }
   )
 
-  if (isLoading)
+  if (true)
   return (
     <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
       <div className='flex-1 flex justify-center items-center flex-col mb-28'>
