d42ab8d744e5cc0195b289d4141063df19e15360
Rohan Rathod <rohan.rthd.58@gmail.com>
2023-11-30 14:05:30 +0530

    Add ChatWrapper OpenAi Message Stream

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -3,16 +3,15 @@
 import { trpc } from '@/app/_trpc/client'
 import ChatInput from "./ChatInput"
 import Messages from "./Messages"
-import { Loader2 } from 'lucide-react'
+import { ChevronLeft, Link, Loader2, XCircle } from 'lucide-react'
+import { buttonVariants } from '../ui/button'
 
 interface ChatWrapperProps {
     fileId: string
-    isSubscribed: boolean
   }
   
 const ChatWrapper = ({
     fileId,
-    isSubscribed,
 }:ChatWrapperProps) => {
 
     const { data, isLoading } =
@@ -29,7 +28,7 @@ const ChatWrapper = ({
       }
   )
 
-  if (true)
+  if (isLoading)
   return (
     <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
       <div className='flex-1 flex justify-center items-center flex-col mb-28'>
@@ -43,14 +42,53 @@ const ChatWrapper = ({
           </p>
         </div>
       </div>
-      <ChatInput/>
+      <ChatInput isDisabled/>
     </div>
   )
 
+  if(data?.status === "PROCESSING") return(
+    <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+    <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 text-blue-500 animate-spin' />
+        <h3 className='font-semibold text-xl'>
+          Processing PDF...
+        </h3>
+        <p className='text-zinc-500 text-sm'>
+          This won&apos;t take to long.
+        </p>
+      </div>
+    </div>
+    <ChatInput isDisabled/>
+  </div>
+  )
 
-
-
-
+  if(data?.status === "FAILED") return (
+    <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+    <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+      <div className='flex flex-col items-center gap-2'>
+        <XCircle className='h-8 w-8 text-red-500' />
+        <h3 className='font-semibold text-xl'>
+          Too many pages in PDF...
+        </h3>
+        <p className='text-zinc-500 text-sm'>
+          Your <span className='font-medium'>Free</span>{' '}
+          plan supports up to 5 pages per PDF
+        </p>
+        <Link
+              href='/dashboard'
+              className={buttonVariants({
+                variant: 'secondary',
+                className: 'mt-4',
+              })}>
+              <ChevronLeft className='h-3 w-3 mr-1.5' />
+              Back
+            </Link>
+      </div>
+    </div>
+    <ChatInput isDisabled/>
+  </div>
+  )
     return <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
         <div className="flex-1 flex justify-center items-center flex-col mb-28">
             <Messages />
